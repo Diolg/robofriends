ec361bac93d4e64e51726fa990c514ed96c952e9
Diolg <olga.diakoff@gmail.com>
2022-12-22 11:07:54 +0000

    Updates

---
+++ precache-manifest.32d4b3f0a6ff626a2f22d8c01c76b452.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "982de1103ba7fbd73346",
+    "url": "/robots/static/css/main.dc834690.chunk.css"
+  },
+  {
+    "revision": "982de1103ba7fbd73346",
+    "url": "/robots/static/js/main.982de110.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robots/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "5ae69d9dc85378d1f6d4",
+    "url": "/robots/static/css/2.56ecc43a.chunk.css"
+  },
+  {
+    "revision": "5ae69d9dc85378d1f6d4",
+    "url": "/robots/static/js/2.5ae69d9d.chunk.js"
+  },
+  {
+    "revision": "babfa971f9a1659ea947",
+    "url": "/robots/static/js/runtime~main.babfa971.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "e2fc855fd6500e12940aa33e11e7696a",
+    "url": "/robots/index.html"
+  }
+];
